0bcb13cc5f0f80cda73f1c605b71b68c58827878
Jason Cameron <git@jasoncameron.dev>
2024-11-24 01:49:56 -0500

    Remove echo

--- dyelog/web/api/echo/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Echo API."""
-
-from dyelog.web.api.echo.views import router
-
-__all__ = ["router"]
--- dyelog/web/api/router.py
+++ dyelog/web/api/router.py
@@ -1,10 +1,9 @@
 from fastapi.routing import APIRouter
 
-from dyelog.web.api import chat, docs, echo, monitoring, speech
+from dyelog.web.api import chat, docs, monitoring, speech
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
 api_router.include_router(chat.router)
 api_router.include_router(speech.router, tags=["speech"])
